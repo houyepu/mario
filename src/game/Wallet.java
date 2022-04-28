4d4de7b7ad2759876addcd6d568626b73003dc0b
houyepu <yhou0023@student.monash.edu>
2022-04-28 10:45:37 +1000

    coin monologue and wallet classes

---
+++ src/game/Wallet.java
@@ -0,0 +1,14 @@
+package game;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actions.DoNothingAction;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.displays.Menu;
+
+import java.util.HashMap;
+import java.util.Map;
+public class Wallet {
+}
