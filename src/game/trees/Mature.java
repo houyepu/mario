bf1122649e237564b0ae19077b3689022e18bbde
Jack <jpat0036@student.monash.edu>
2022-04-30 22:05:07 +1000

    Feature: Mature trees now spawn Koopas, will sometimes wither and die.

--- src/game/trees/Mature.java
+++ src/game/trees/Mature.java
@@ -2,6 +2,12 @@ package game.trees;
 
 
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.Dirt;
+import game.enemies.Koopa;
+import game.wallet.Coin;
+
+import java.util.Random;
 
 public class Mature extends Ground {
 
@@ -9,5 +15,20 @@ public class Mature extends Ground {
         super('T');
     }
 
+    Random rand = new Random();
+    int turnsAlive;
 
+    @Override
+    public void tick(Location location) {
+        turnsAlive += 1;
+        if (rand.nextInt(100) <= 20) {
+            System.out.println("A MATURE TREE HAS DIED"); //testing purposes only
+            location.setGround(new Dirt());
+        }
+        if (rand.nextInt(100) <= 10) {
+            location.addActor(new Koopa());
+            System.out.println("Koopa Spawned!");
+        }
+
+    }
 }
