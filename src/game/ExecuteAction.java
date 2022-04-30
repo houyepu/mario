f77a6ff33b8e1636657f20677f4e06430c51e2b3
joel_atta <jatt0005@student.monash.edu>
2022-04-30 16:21:45 +1000

    REQ3: Basic changes to Koopa and ExecuteAction

--- src/game/ExecuteAction.java
+++ src/game/ExecuteAction.java
@@ -5,6 +5,15 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
 
 public class ExecuteAction extends Action {
+
+    protected Actor target;
+    protected String direction;
+
+    public ExecuteAction(Actor target, String direction) {
+        this.target = target;
+        this.direction = direction;
+    }
+
     @Override
     public String execute(Actor actor, GameMap map) {
         return null;
--- src/game/enemies/Koopa.java
+++ src/game/enemies/Koopa.java
@@ -2,23 +2,44 @@ package game.enemies;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actions.DoNothingAction;
+import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
+import game.*;
 
-public class Koopa extends Enemy{
-    /**
-     * Constructor.
-     *
-     * @param name        the name of the Actor
-     * @param displayChar the character that will represent the Actor in the display
-     * @param hitPoints   the Actor's starting hit points
-     */
-    public Koopa(String name, char displayChar, int hitPoints) {
-        super(name, displayChar, hitPoints);
+public class Koopa extends Enemy {
+
+    public Koopa() {
+        super("Koopa", 'K', 100);
+        this.behaviours.put(10, new WanderBehaviour());
+        this.behaviours.put(20, new AttackBehaviour());
+    }
+
+    @Override
+    protected IntrinsicWeapon getIntrinsicWeapon() {
+        return new IntrinsicWeapon(30, "punches");
+    }
+
+    @Override
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+        ActionList actions = new ActionList();
+        // it can be attacked only by the HOSTILE opponent, and this action will not attack the HOSTILE enemy back.
+        if (otherActor.hasCapability(Status.HOSTILE_TO_ENEMY)) {
+            actions.add(new AttackAction(this, direction));
+            actions.add(new ExecuteAction(this, direction));
+        }
+        return actions;
     }
 
     @Override
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
-        return null;
+        for (Behaviour Behaviour : behaviours.values()) {
+            Action action = Behaviour.getAction(this, map);
+            if (action != null)
+                return action;
+        }
+        return new DoNothingAction();
     }
 }
