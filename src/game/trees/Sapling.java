ab8e14cf1876879dea47c0ef73879bf6c36de6fd
Jack <jpat0036@student.monash.edu>
2022-04-30 21:54:00 +1000

    Feature: Saplings now sometimes spawn coins.

--- src/game/trees/Sapling.java
+++ src/game/trees/Sapling.java
@@ -2,6 +2,10 @@ package game.trees;
 
 import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
+import game.enemies.Goomba;
+import game.wallet.Coin;
+
+import java.util.Random;
 
 public class Sapling extends Ground {
 
@@ -9,13 +13,20 @@ public class Sapling extends Ground {
         super('t');
     }
 
+    Random rand = new Random();
     int turnsAlive;
+
     @Override
     public void tick(Location location) {
-        turnsAlive+=1;
-        if(turnsAlive>=9) { //after 10 turns, time to move to the next stage
-            System.out.println("SAPLING GROW UP");
+        turnsAlive += 1;
+        if (turnsAlive >= 9) { //after 10 turns, time to move to the next stage
+            System.out.println("SAPLINGS GROW UP"); //testing purposes only
             location.setGround(new Mature());
         }
+        if (rand.nextInt(100) <= 10) {
+            location.addItem(new Coin("coin",'$',true,20));
+            System.out.println("Money tree!");
+        }
+
     }
 }
