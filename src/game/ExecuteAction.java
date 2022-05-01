0fdeefb8371cc0a2d1f5335f84e5e630876f0fae
joel_atta <jatt0005@student.monash.edu>
2022-05-02 00:06:34 +1000

    REQ3: Action to break Koopa's shell (execute) implemented

--- src/game/ExecuteAction.java
+++ src/game/ExecuteAction.java
@@ -1,8 +1,11 @@
 package game;
 
 import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
+import game.enemies.Koopa;
 
 public class ExecuteAction extends Action {
 
@@ -16,11 +19,28 @@ public class ExecuteAction extends Action {
 
     @Override
     public String execute(Actor actor, GameMap map) {
-        return null;
+        String result;
+
+        if (!target.isConscious() && target.hasCapability(Status.DORMANT) && actor.getWeapon().verb().equals("wrenches")){
+            ActionList dropActions = new ActionList();
+            // drop all items
+            for (Item item : target.getInventory())
+                dropActions.add(item.getDropAction(actor));
+            for (Action drop : dropActions)
+                drop.execute(target, map);
+            // remove actor
+            map.removeActor(target);
+            result = target + " is executed.";
+        }
+        else {
+            result = target + " cannot be executed at the moment.";
+        }
+
+        return result;
     }
 
     @Override
     public String menuDescription(Actor actor) {
-        return null;
+        return actor + " executes " + target + " at " + direction;
     }
 }
